18c7d978a1974370f726a74e5a72ca1229002a67
lavp999 <lavp99@hotmail.com>
2022-11-05 17:37:58 +0000

    haSTA EL 4.2

--- .learn/resets/03.5-alert-component/app.jsx
+++ .learn/resets/03.5-alert-component/app.jsx
@@ -6,13 +6,7 @@ import PropTypes from "prop-types";
  * here you have declare your Alert component and return
  * the html that bootstrap dictates for its alert component
  */
-const Alert = () => {
-	return (
-		<div class="alert alert-danger" role="alert">
-			OMG! Something really bad has happended!
-		</div>
-	);
-};
+
 
 // here is where the alert component is being used, you don't have to edit this part,
 // but it helps you understan what properties is the component using
--- .learn/resets/04.2-conditional-rendering/app.jsx
+++ .learn/resets/04.2-conditional-rendering/app.jsx
@@ -3,11 +3,10 @@ import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
 // add the green inside this function
-const Alert = (props) => {
+const Alert = props => {
 	const colorClasses = {
 		red: "alert-danger",
-		orange: "alert-warning",
-		green: "alert-success",
+		orange: "alert-warning"
 	};
 	if (colorClasses[props.color] === undefined) alert(`The color ${props.color} is not in the possible list of colors`);
 
@@ -19,7 +18,7 @@ const Alert = (props) => {
 };
 Alert.propTypes = {
 	color: PropTypes.string,
-	text: PropTypes.string,
+	text: PropTypes.string
 };
 
 // here is where the alert component is being used, you don't have to edit this part,
@@ -29,7 +28,6 @@ ReactDOM.render(
 		<Alert text="OMG! Something really bad has happended!" color="red" />
 		<Alert text="Well, it is not that bad after all!" color="orange" />
 		<Alert text="I am supposed to be green" color="green" />
-		<Alert text="I am supposed to be verde" color="verde" />
 	</div>,
 	document.querySelector("#myDiv")
 );
